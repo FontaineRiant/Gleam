f8ff31a8f7abefad6bb683d638be57903a739f4b
Antoine Friant <antoine.friant@hotmail.com>
2018-05-06 16:19:16 +0200

    training works on gpu

--- Code/cnn_pop/train.py
+++ Code/cnn_pop/train.py
@@ -1,4 +1,3 @@
-# nécessite environ 10 Go de RAM disponible, peut prendre jusqu'à 1h
 
 import pandas as pd
 import rasterio
@@ -9,59 +8,70 @@ import keras.layers.convolutional as conv
 import keras.models as models
 import keras.utils.np_utils as kutils
 
+tile_size = 32
+
 def normalize(array):
-    array = np.array(array).astype(np.float)
+    array = np.array(array).astype('float32')
     array = (array - np.amin(array)) / (np.amax(array) - np.amin(array))
     return array
+
+def tile(matrix):
+    y = 0
+    tiles = []
+    while (y + tile_size < matrix.shape[1]):
+        x = 0
+        while(x + tile_size < matrix.shape[0]):
+            tiles.append(matrix[x: x + tile_size, y: y + tile_size])
+            x += tile_size
+        y += tile_size
+    tiles = np.array(tiles)
+    return tiles[:100]
 
 print('opening raster')
 
 train = rasterio.open('../../Data/lightpop_merged/2000_subset.tif')
-trainX = normalize(train.read(1))
-trainY = normalize(train.read(2))
+trainX = np.expand_dims(tile(normalize(train.read(1))), axis=3)
+trainY = np.expand_dims(tile(normalize(train.read(2))), axis=3)
 
 test = rasterio.open('../../Data/lightpop_merged/2005_subset.tif')
-testX = normalize(train.read(1))
-testY = normalize(train.read(2))
+testX = np.expand_dims(tile(normalize(test.read(1))), axis=3)
+testY = np.expand_dims(tile(normalize(test.read(2))), axis=3)
+
+print(trainX.shape)
+img_count, img_rows, img_cols, img_channel_count = trainX.shape
+print('image shape : ' + str(trainX.shape))
 
 print('configuring cnn')
 
 nb_epoch = 1 # Change to 100
 
-batch_size = 128
-img_rows, img_cols = testX
+batch_size = 1
+
 
 # last filter makes the input layer for the last perceptron bigger
-nb_filters_1 = 32
+nb_filters_1 = 2
 nb_filters_2 = 64
-nb_conv_1 = 32
-nb_conv_2 = 8
+nb_conv_1 = 5
+nb_conv_2 = 4
 
 cnn = models.Sequential()
+cnn.add(conv.Convolution2D(filters=nb_filters_1, kernel_size=(nb_conv_1, nb_conv_1), activation="relu", border_mode='same',
+    input_shape=(img_rows, img_cols, img_channel_count)))
+#cnn.add(conv.MaxPooling2D(strides=(2,2)))
 
-cnn.add(conv.Convolution2D(nb_filters_1, nb_conv_1, nb_conv_1,  activation="relu", input_shape=(img_rows, img_cols, 1), 
-    border_mode='same'))
-cnn.add(conv.Convolution2D(nb_filters_1, nb_conv_1, nb_conv_1, activation="relu", border_mode='same'))
-cnn.add(conv.MaxPooling2D(strides=(2,2)))
+#cnn.add(conv.Convolution2D(filters=nb_filters_2, kernel_size=(nb_conv_2, nb_conv_2), activation="relu", border_mode='same'))
+#cnn.add(conv.MaxPooling2D(strides=(2,2)))
 
-cnn.add(conv.Convolution2D(nb_filters_2, nb_conv_2, nb_conv_2, activation="relu", border_mode='same'))
-cnn.add(conv.Convolution2D(nb_filters_2, nb_conv_2, nb_conv_2, activation="relu", border_mode='same'))
-cnn.add(conv.MaxPooling2D(strides=(2,2)))
-
-cnn.add(core.Flatten())
-cnn.add(core.Dropout(0.2))
-cnn.add(core.Dense(4096, activation=None))
+#cnn.add(core.Dropout(0.2))
+cnn.add(core.Dense(1))
 
 cnn.summary()
 cnn.compile(loss="mean_squared_error", optimizer="adam", metrics=["mse"])
 
 print('training')
 
-cnn.fit(trainX, trainY, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1)
+cnn.fit(trainX, trainY, batch_size=batch_size, epochs=nb_epoch, verbose=1)
 
-yPred = cnn.predict_classes(testX)
-
-np.savetxt('trained.csv', np.c_[range(1,len(yPred)+1),yPred], delimiter=',',
-    header = 'ImageId,Label', comments = '', fmt='%d')
+cnn.save('model.h5')
 
 print('done !')
