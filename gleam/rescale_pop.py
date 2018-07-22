7e83a2ca79f889643e2c9b076589a9a92f724955
Antoine Friant <antoine.friant@hotmail.com>
2018-07-22 14:52:14 +0200

    rapport

--- gleam/rescale_pop.py
+++ gleam/rescale_pop.py
@@ -4,7 +4,7 @@ import numpy as np
 ## PARAMETERS ##
 directory = '../data/lightpop_merged/'
 
-input_files = ['2015_usa.tif', '2015_italy.tif']
+input_files = ['2015_portugal.tif']
 
 divide_pop_by = 4
 ################
